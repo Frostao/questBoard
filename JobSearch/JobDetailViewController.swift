2a878c47bc18b8117d8dc34e0ddcaf0afb86168b
Grey Chen <geleichen@gmail.com>
2015-03-18 16:34:00 -0400

    add Quest detail view

--- JobSearch/JobDetailViewController.swift
+++ JobSearch/JobDetailViewController.swift
@@ -12,71 +12,11 @@ import MapKit
 class JobDetailViewController: UIViewController,UIAlertViewDelegate {
     
     
-    @IBAction func deletePressed(sender: UIButton) {
-        //postid token
-        let alertBigController = UIAlertController(title: "Please conform", message: "Are you sure you want to continue?", preferredStyle: .Alert)
-        
-        
-        
-        let yes = UIAlertAction(title: "Yes", style: .Default) { (_) in
-            let defaults = NSUserDefaults.standardUserDefaults()
-            if let token:String = defaults.valueForKey("token") as? String {
-                SIOSocket.socketWithHost("http://nerved.herokuapp.com", response: { (socket:SIOSocket!) in
-                    self.socket = socket;
-                    let dict = NSDictionary(objectsAndKeys: token,"token",self.currentJob!.postID,"postid")
-                    self.socket.emit("delete", args: [dict])
-                    self.socket.on("response", callback: { (args:[AnyObject]!)  in
-                        let arg = args as SIOParameterArray
-                        let dict = arg[0] as NSDictionary
-                        println(dict)
-                        if dict.objectForKey("message") as String == "post delete failed" {
-                            let alertController = UIAlertController(title: "Sorry", message: "Since you are not the poster of this quest, you can't delete this quest", preferredStyle: .Alert)
-                            let ok = UIAlertAction(title: "OK", style: .Default) { (_) in
-                                
-                                
-                            }
-                            
-                            alertController.addAction(ok)
-                            self.presentViewController(alertController, animated: true, completion: nil)
-                        } else {
-                            let alertController = UIAlertController(title: "Successful", message: "You have successfully deleted the quest", preferredStyle: .Alert)
-                            let ok = UIAlertAction(title: "OK", style: .Default) { (_) in
-                            NSNotificationCenter.defaultCenter().postNotificationName("addedJob", object: nil)
-                                self.navigationController?.popViewControllerAnimated(true)
-                                
-                            }
-                            
-                            alertController.addAction(ok)
-                            self.presentViewController(alertController, animated: true, completion: nil)
-                        }
-                    })
-                })
-            } else {
-                self.performSegueWithIdentifier("showLogin", sender: self)
-            }
-            
-            
-        }
-        
-        let cancel = UIAlertAction(title: "No", style: .Default) { (_) in
-            
-            
-        }
-        alertBigController.addAction(yes)
-        alertBigController.addAction(cancel)
-        self.presentViewController(alertBigController, animated: true, completion: nil)
-        
-        
-        
-        
-        
-    }
     var currentJob:Job?
     var socket = SIOSocket()
     var phoneNumber : String?
     var email : String?
     @IBOutlet weak var endDate: UILabel!
-    @IBOutlet weak var delete: UIButton!
     @IBOutlet weak var apply: UIButton!
     @IBAction func apply(sender: UIButton) {
         let defaults = NSUserDefaults.standardUserDefaults()
@@ -143,8 +83,7 @@ class JobDetailViewController: UIViewController,UIAlertViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.delete.backgroundColor = UIColor(red: 255/255.0, green: 110/255.0, blue: 128/255.0, alpha: 1)
-        self.delete.tintColor = UIColor(red: 245, green: 146, blue: 108, alpha: 1)
+        
         SIOSocket.socketWithHost("http://nerved.herokuapp.com", response: { (socket:SIOSocket!) in
             self.socket = socket;
             let dict = NSDictionary(objectsAndKeys: self.currentJob!.UUID,"uuid")
---
+++ JobSearch/QuestDetailViewController.swift
@@ -0,0 +1,113 @@
+//
+//  QuestDetailViewController.swift
+//  
+//
+//  Created by Gelei Chen on 15/3/18.
+//
+//
+
+import UIKit
+
+class QuestDetailViewController: UIViewController {
+
+   
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var endDate: UILabel!
+    @IBOutlet weak var salary: UILabel!
+    @IBOutlet weak var jobDescription: UILabel!
+    @IBOutlet weak var postTime: UILabel!
+    @IBOutlet weak var jobID: UILabel!
+    @IBOutlet weak var jobTitle: UILabel!
+    @IBOutlet weak var delete: UIButton!
+    @IBOutlet weak var edit: UIButton!
+    @IBAction func deletePressed(sender: UIButton) {
+        //postid token
+        let alertBigController = UIAlertController(title: "Please conform", message: "Are you sure you want to continue?", preferredStyle: .Alert)
+        
+        
+        
+        let yes = UIAlertAction(title: "Yes", style: .Default) { (_) in
+            let defaults = NSUserDefaults.standardUserDefaults()
+            if let token:String = defaults.valueForKey("token") as? String {
+                SIOSocket.socketWithHost("http://nerved.herokuapp.com", response: { (socket:SIOSocket!) in
+                    self.socket = socket;
+                    let dict = NSDictionary(objectsAndKeys: token,"token",self.currentJob!.postID,"postid")
+                    self.socket.emit("delete", args: [dict])
+                    self.socket.on("response", callback: { (args:[AnyObject]!)  in
+                        let arg = args as SIOParameterArray
+                        let dict = arg[0] as NSDictionary
+                        println(dict)
+                        if dict.objectForKey("message") as String == "post delete failed" {
+                            let alertController = UIAlertController(title: "Sorry", message: "Since you are not the poster of this quest, you can't delete this quest", preferredStyle: .Alert)
+                            let ok = UIAlertAction(title: "OK", style: .Default) { (_) in
+                                
+                                
+                            }
+                            
+                            alertController.addAction(ok)
+                            self.presentViewController(alertController, animated: true, completion: nil)
+                        } else {
+                            let alertController = UIAlertController(title: "Successful", message: "You have successfully deleted the quest", preferredStyle: .Alert)
+                            let ok = UIAlertAction(title: "OK", style: .Default) { (_) in
+                                NSNotificationCenter.defaultCenter().postNotificationName("addedJob", object: nil)
+                                self.navigationController?.popViewControllerAnimated(true)
+                                
+                            }
+                            
+                            alertController.addAction(ok)
+                            self.presentViewController(alertController, animated: true, completion: nil)
+                        }
+                    })
+                })
+            } else {
+                self.performSegueWithIdentifier("showLogin", sender: self)
+            }
+            
+            
+        }
+        
+        let cancel = UIAlertAction(title: "No", style: .Default) { (_) in
+            
+            
+        }
+        alertBigController.addAction(yes)
+        alertBigController.addAction(cancel)
+        self.presentViewController(alertBigController, animated: true, completion: nil)
+        
+        
+        
+        
+        
+    }
+    
+    @IBAction func edit(sender: UIButton) {
+    }
+    
+    var currentJob:Job?
+    var socket = SIOSocket()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delete.backgroundColor = UIColor(red: 255/255.0, green: 110/255.0, blue: 128/255.0, alpha: 1)
+        self.delete.tintColor = UIColor(red: 245, green: 146, blue: 108, alpha: 1)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
